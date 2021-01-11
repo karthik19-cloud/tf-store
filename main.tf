77189a167207f7bb1be08bb9e7a14fac7a40cac7
prasadjblin <prasadjblin@gmail.com>
2021-01-12 00:49:55 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,27 @@
+provider "google" {
+	version = "3.5.0"
+	credentials = file("cloudworks-252411-74141a6894a2.json")
+	project = "cloudworks-252411"
+	region  = "us-central1"
+	zone    = "us-central1-c"	
+}
+
+# Our logged compute instance
+resource "google_compute_instance" "my-logged-instance" {
+  name         = "my-instance"
+  machine_type = "e2-medium"
+  zone         = "us-central1-c"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+    }
+  }
+}
